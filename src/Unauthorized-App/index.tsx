a7e5a5f6b2a62a2f069d5bcca57ef2666870360f
Tcgogo <1812497229@qq.com>
2021-11-15 22:57:13 +0800

    chore(antd): 引入antd，配置主题色

--- src/Unauthorized-App/index.tsx
+++ src/Unauthorized-App/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Login from "./login";
 import Register from "./register";
+import "./index.css";
 
 const Unauthorized = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="w100">
+    <div className="loginBox w100">
       {isLogin ? <Login /> : <Register />}
       <div onClick={() => setIsLogin(!isLogin)}>
         切换到{isLogin ? "注册" : "登录"}
--- src/Unauthorized-App/login.tsx
+++ src/Unauthorized-App/login.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from "context/auth-context";
 import React, { FormEvent, useState } from "react";
-import { apiUrl } from "../auth-provider";
+import { Button, Form, Input } from "antd";
 
 const Login = () => {
   const context = useAuth();
@@ -8,15 +8,8 @@ const Login = () => {
   const [isLog, setIsLog] = useState<boolean>(false);
 
   // 登录
-  const onsubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const resDoms = Array.from(e.currentTarget.elements).filter((e) => {
-      return e.id === "password" || e.id === "username";
-    }) as HTMLInputElement[];
-    const username = resDoms[0].value;
-    const password = resDoms[1].value;
-
+  const onsubmit = async (value: { username: string; password: string }) => {
+    const { username, password } = value;
     const data = await context.login({
       username,
       password,
@@ -26,15 +19,26 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={onsubmit} className="flex-column h100">
+    <Form onFinish={onsubmit} className="flex-column h100">
       {isLog ? <div>登录成功，token为{context.user?.token}</div> : null}
-      <label htmlFor="username">账号</label>
-      <input id={"username"} type="text" />
-      <br />
-      <label htmlFor="password">密码</label>
-      <input id={"password"} type="password" />
-      <button>登陆</button>
-    </form>
+      <Form.Item
+        name={"username"}
+        label="账号"
+        rules={[{ required: true, message: "请输入账号" }]}
+      >
+        <Input></Input>
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        label="密码"
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input type="password"></Input>
+      </Form.Item>
+      <Button htmlType="submit" type="primary">
+        登陆
+      </Button>
+    </Form>
   );
 };
 
